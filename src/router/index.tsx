d31e9a33e5e236411823be2b757177cd217fd9da
Wérick Santos <202411240021@ifba.edu.br>
2025-12-12 22:42:07 -0300

    Feat: Integra api com o frontend

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -11,9 +11,11 @@ import AddParticipantsPage from "../features/participants/screens/AddParticipant
 import { GroupsPage } from "../features/groups/screens/GroupsPage.tsx";
 import EditPerfil from "../features/editPerfil/screens/EditPerfil.tsx";
 import AddFriends from "../features/addFriends/screens/AddFriends.tsx";
+import ViewGroupPage from "../features/groups/screens/ViewGroupPage.tsx";
 import MembersManagementPage from "../features/member/screens/MembersManagementPage.tsx";
 import MemberHistoryPage from "../features/history/screens/MemberHistoryPage.tsx";
 import MemberAdmin from "../features/memberAdmin/screens/MemberAdmin.tsx";
+import GroupPaymentsPage from "../features/groups/screens/GroupPaymentsPage.tsx";
 import { ExpenseSummaryPage } from "../features/expenses/components/ExpenseSummaryPage.tsx";
 import { ProtectedRoute } from "../components/auth/index.tsx";
 import { Navigate } from "react-router-dom";
@@ -38,10 +40,12 @@ export const AppRouter = () => {
                 <Route path="/criar-novo-grupo" element={<CreateGroupPage />} />
                 <Route path="/editar-grupo" element={<EditGroupPage />} />
                 <Route path="/editar-grupo/:groupId" element={<EditGroupPage />} />
+                <Route path="/ver-grupo/:groupId" element={<ViewGroupPage />} />
 
                 <Route path="/grupo/membro" element={<MembersManagementPage />} />
                 <Route path="/grupo/membro/admin/:groupId" element={<MemberAdmin />} />
                 <Route path="/grupo/:groupId/historico/:memberId" element={<MemberHistoryPage />} />
+                <Route path="/grupo/:groupId/pagamentos" element={<GroupPaymentsPage />} />
                 <Route
                   path="/adicionar-participantes"
                   element={<AddParticipantsPage />}
